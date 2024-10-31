4893032dd210121307317dbc74f9c1a1d16aae08
shabibahmed11 <shabibsahmed@gmail.com>
2024-10-31 17:06:34 -0400

    2 Clustering Methods

--- Project2.py
+++ Project2.py
@@ -5,7 +5,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.metrics.pairwise import linear_kernel
-
+from sklearn.cluster import DBSCAN
+from sklearn.metrics import silhouette_score
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 textArray = []
 
 def readAndProcess(fileName):
@@ -23,7 +26,12 @@ def bagOfWordsCreator(textArray):
                 file.write(str(i)) 
         """
         return BOW
-
+def plotClusters(simplifiedCLusters, labels, title):
+    plt.figure(figsize=(10, 5))
+    plt.scatter(simplifiedCLusters[:, 0], simplifiedCLusters[:, 1], c=labels, cmap='viridis', s=2)
+    plt.title(title)
+    plt.show()
+    
 def cosineDistance(bagOfWords):
     cosineSimilarities = linear_kernel(bagOfWords, bagOfWords)
     cosineSimilarities = cosineSimilarities.flatten()
@@ -47,8 +55,29 @@ def euclideanDistance(bagOfWords):
     """
     plt.hist(euclideanSimiliarties, bins = 500)
     plt.show()
+
+def dbscanCosine(bagOfWords):
+    dbscan = DBSCAN(eps=0.5, min_samples=5, metric='cosine')
+    dbscan.fit(bagOfWords)
+    labels = dbscan.labels_
+    print(labels)
+    silScore = silhouette_score(bagOfWords, labels)
+    print(f'Silhouette Score for Cosine DBSCAN: {silScore}')
+    simplifiedCLusters = PCA(n_components=2).fit_transform(bagOfWords.toarray())
+    plotClusters(simplifiedCLusters,labels, 'DBSCAN Cosine Clusters')
+    
+def kMeans(bagOfWords):
+    num_clusters = 20
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42)
+    kmeans.fit(bagOfWords)
+    labels = kmeans.labels_
+    print(labels)
+    silScore = silhouette_score(bagOfWords, labels)
+    print(f'Silhouette Score for Kmeans: {silScore}')
+    simplifiedCLusters = PCA(n_components=2).fit_transform(bagOfWords.toarray())
+    plotClusters(simplifiedCLusters,labels, 'Kmeans Clusters')
     
 readAndProcess("cnnhealth.txt")
 bagOfWords = bagOfWordsCreator(textArray)
-cosineDistance(bagOfWords)
-euclideanDistance(bagOfWords)
+
+kMeans(bagOfWords)
